f9cf0ae250aea94027eee747bd7e912716ce01f8
ibrahimelgendy100 <ibrahimelgendy100@gmail.com>
2021-12-12 04:36:42 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+	public class Owner : BaseEntity
+	{
+		public string FullName { get; set; }
+		public string Profil { get; set; }
+		public string PictureUrl { get; set; }
+		public Address Address { get; set; }
+	}
+}
